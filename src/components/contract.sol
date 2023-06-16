4a6b703582cb498f9b2304202b8b987237ecf109
Abhishekkrishnan6 <ak6726084@gmail.com>
2023-06-16 11:47:00 +0530

    contract completed

--- src/components/contract.sol
+++ src/components/contract.sol
@@ -1,4 +1,35 @@
+// SPDX-License-Identifier: MIT 
 pragma solidity >0.7.0 <=0.9.0;
+
+contract CampaignFactory{
+    address[] public deloyedCampaigns;
+    event campaigncreated(
+        string title,
+        uint requiredamount,
+        address indexed owner,
+        address campaignaddress,
+        string imgurl,
+        uint indexed timestamp,
+        string indexed category
+    );
+    function createCampaign(
+        string memory campaigntitle,
+        uint requiredcampaignamount,
+        string memory imgurl,
+        string memory category,
+        string memory storyurl) public
+        {
+            campaign newcampaign = new campaign(
+                campaigntitle,
+                requiredcampaignamount,
+                imgurl,
+                storyurl
+            );
+            deloyedCampaigns.push(address(newcampaign));
+            emit campaigncreated(campaigntitle, requiredcampaignamount, msg.sender, address(newcampaign), imgurl, block.timestamp, category);
+        }
+}
+
 contract campaign{
     string public title;
     uint public requiredamout;
