7ecff29091ba3d95a1bc4c6d56b9bd91dfd8df4d
Abhishekkrishnan6 <ak6726084@gmail.com>
2023-06-18 17:59:19 +0530

    usenavigate

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useNavigate , Link } from 'react-router-dom'
 
 const Error = () => {
+    const navigate = useNavigate();
   return (
+    <>
     <div>404 Error Page not found</div>
+    <button onClick={()=> navigate(-1)}>Go Back</button>
+    </>
   )
 }
 
